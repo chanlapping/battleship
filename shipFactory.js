63353b11122bfbef1685fc1af0373ecf9913dcc2
chan lap ping <chanlapping64@gmail.com>
2023-05-04 21:30:10 +0800

    Set up Jest, Add shipFactory function and its tests.

---
+++ shipFactory.js
@@ -0,0 +1,27 @@
+function shipFactory(type) {
+    let hits = 0;
+    let length = 0;
+    if (type == 'carrier') {
+        length = 5;
+    } else if (type == 'battleship') {
+        length = 4;
+    } else if (type == 'destroyer') {
+        length = 3;
+    } else if (type == 'submarine') {
+        length = 3;
+    } else if (type == 'patrol') {
+        length = 2;
+    }
+
+    const isSunk = () => {
+        return hits == length;
+    }
+
+    const hit = () => {
+        hits++;
+    }
+
+    return {isSunk, hit};
+}
+
+module.exports = shipFactory;
---
+++ shipFactory.test.js
@@ -0,0 +1,21 @@
+const shipFactory = require('./shipFactory');
+
+test('new ship is not sunk', () => {
+    const patrol = shipFactory('patrol');
+    expect(patrol.isSunk()).toBe(false);
+});
+
+test('when hit enough times the ship is sunk', () => {
+    const submarine = shipFactory('submarine');
+    submarine.hit();
+    submarine.hit();
+    submarine.hit();
+    expect(submarine.isSunk()).toBe(true);
+});
+
+test('when hit not enough times the ship is not sunk', () => {
+    const submarine = shipFactory('submarine');
+    submarine.hit();
+    submarine.hit();
+    expect(submarine.isSunk()).toBe(false);
+});
