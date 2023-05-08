1734c8dd93cb7f0a5a1fb92ae7507c7e5c069c41
chan lap ping <chanlapping64@gmail.com>
2023-05-08 10:37:13 +0800

    Refactor test on gameBoard, use mock beforeEach and mock

--- src/gameBoardFactory.test.js
+++ src/gameBoardFactory.test.js
@@ -1,24 +1,27 @@
 import { experiments } from "webpack";
 import gameBoardFactory from "./gameBoardFactory";
+import shipFactory from "./shipFactory";
 
+let gameBoard;
 
+beforeEach(() => gameBoard = gameBoardFactory());
 
 test('places ship on correct position', () => {
-    const gameBoard = gameBoardFactory();
+    
     gameBoard.placeShip('patrol', 'h', 0, 0); // h: horizontal, v: vertical
     expect(gameBoard.get(0, 0)).toBe(0);
     expect(gameBoard.get(1, 0)).toBe(0);
 });
 
 test('places ship with correct direction', () => {
-    const gameBoard = gameBoardFactory();
+    
     gameBoard.placeShip('patrol', 'v', 0, 0);
     expect(gameBoard.get(0, 0)).toBe(0);
     expect(gameBoard.get(0, 1)).toBe(0);
 });
 
 test('places correct ship number on position', () => {
-    const gameBoard = gameBoardFactory();
+    
     gameBoard.placeShip('patrol', 'h', 0, 0);
     gameBoard.placeShip('carrier', 'v', 2, 2);
     expect(gameBoard.get(0, 0)).toBe(0);
@@ -31,7 +34,7 @@ test('places correct ship number on position', () => {
 });
 
 test('cannot place ship if there is not enough space', () => {
-    const gameBoard = gameBoardFactory();
+    
     gameBoard.placeShip('carrier', 'h', 6, 0);
     expect(gameBoard.get(6, 0)).toBe('e');
     expect(gameBoard.get(7, 0)).toBe('e');
@@ -40,7 +43,7 @@ test('cannot place ship if there is not enough space', () => {
 });
 
 test('cannot place ship if there is not enough space, vertical', () => {
-    const gameBoard = gameBoardFactory();
+    
     gameBoard.placeShip('carrier', 'v', 0, 6);
     expect(gameBoard.get(0, 6)).toBe('e');
     expect(gameBoard.get(0, 7)).toBe('e');
@@ -49,7 +52,7 @@ test('cannot place ship if there is not enough space, vertical', () => {
 });
 
 test('cannot place ship if overlaps with another ship', () => {
-    const gameBoard = gameBoardFactory();
+    
     gameBoard.placeShip('destroyer', 'h', 2, 2);
     gameBoard.placeShip('destroyer', 'v', 3, 2);
     expect(gameBoard.get(3, 2)).toBe(0);
@@ -58,21 +61,22 @@ test('cannot place ship if overlaps with another ship', () => {
 });
 
 test('records a miss if the square is empty', () => {
-    const gameBoard = gameBoardFactory();
+    
     gameBoard.receiveAttack(0, 0);
     expect(gameBoard.get(0, 0)).toBe('m');
 });
 
 test('if hit, calls the correct ship hit method', () => {
-    const gameBoard = gameBoardFactory();
+    
     gameBoard.placeShip('patrol', 'h', 0, 0);
+    jest.spyOn(gameBoard.getShip(0), "hit");
     gameBoard.receiveAttack(1, 0);
     expect(gameBoard.get(1, 0)).toBe('x');
-    expect(gameBoard.getShip(0).getNumberOfHits()).toBe(1);
+    expect(gameBoard.getShip(0).hit).toHaveBeenCalledTimes(1);
 });
 
 test('reports all ships are sunk', () => {
-    const gameBoard  = gameBoardFactory();
+    
     gameBoard.placeShip('patrol', 'h', 0, 0);
     gameBoard.placeShip('destroyer', 'h', 1, 1);
     gameBoard.receiveAttack(0, 0);
@@ -84,7 +88,7 @@ test('reports all ships are sunk', () => {
 });
 
 test('if not all sunk, reports false', () => {
-    const gameBoard  = gameBoardFactory();
+    
     gameBoard.placeShip('patrol', 'h', 0, 0);
     gameBoard.placeShip('destroyer', 'h', 1, 1);
     gameBoard.receiveAttack(0, 0);
