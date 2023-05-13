10cf6498b59b0b743d43a456843654453d25a82a
chan lap ping <chanlapping64@gmail.com>
2023-05-13 20:02:52 +0800

    Add HTML and CSS files

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import gameBoardFactory from "./gameBoardFactory";
+import playerFactory from "./playerFactory";
+import './style.css';
+
+// create new game
+
+//create game boards
+const playerBoard = gameBoardFactory();
+const computerBoard = gameBoardFactory();
+
+//create players
+const player = playerFactory();
+const computer = playerFactory();
+
+// populate game boards
+playerBoard.placeShip('patrol', 'h', 5, 4);
+playerBoard.placeShip('submarine', 'h', 0, 7);
+playerBoard.placeShip('destroyer', 'v', 6, 7);
+playerBoard.placeShip('battleship', 'v', 8, 3);
+playerBoard.placeShip('carrier', 'v', 3, 1);
+
+computerBoard.placeShip('patrol', 'h', 1, 1);
+computerBoard.placeShip('submarine', 'h', 0, 9);
+computerBoard.placeShip('destroyer', 'v', 2, 3);
+computerBoard.placeShip('battleship', 'h', 4, 2);
+computerBoard.placeShip('carrier', 'v', 6, 4);
